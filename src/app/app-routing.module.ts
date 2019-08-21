acdbdf58624fa0814d624747188def3573cf8fb2
stabiloBOSS96 <Lukas.ziefle@web.de>
2019-08-22 01:28:02 +0200

    Implemented Login User

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AllProductsComponent } from './all-products/all-products.component';
 import { FlightDetailComponent } from './flight-detail/flight-detail.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
@@ -17,6 +18,10 @@ const routes: Routes = [
   {
     path: 'flight-detail/:id',
     component: FlightDetailComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   }
 ];
 
--- src/app/header-nav/header-nav.component.ts
+++ src/app/header-nav/header-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../shared/models/User';
 
 @Component({
   selector: 'header-nav',
@@ -10,6 +11,19 @@ export class HeaderNavComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  
+  isLoggedIn() {
+    let loggedIn: boolean = false;
+    if (localStorage.getItem("currentUser") == null) {
+      loggedIn = true;      
+    }
+    return loggedIn;
+  }
+
+  logout(){
+    localStorage.clear()
   }
 
 }
