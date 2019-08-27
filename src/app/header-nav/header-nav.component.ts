a274bad5a2ec017978c670f6390b4bc026a9c154
stabiloBOSS96 <Lukas.ziefle@web.de>
2019-08-27 14:40:27 +0200

    Final commit

--- src/app/header-nav/header-nav.component.ts
+++ src/app/header-nav/header-nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../shared/models/User';
 import { NotifierService } from 'angular-notifier';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'header-nav',
@@ -13,29 +14,41 @@ export class HeaderNavComponent implements OnInit {
 
   constructor(
     notifierService: NotifierService
-  ) { 
+  ) {
     this.notifier = notifierService;
   }
 
   ngOnInit() {
   }
 
-  
+
   isLoggedIn() {
     let loggedIn: boolean = false;
     if (localStorage.getItem("currentUser") == null) {
-      loggedIn = true;      
+      loggedIn = true;
     }
     return loggedIn;
   }
 
-  logout(){
+  logout() {
     console.log(localStorage.getItem("currentUser"));
-    
+
     let user: User = JSON.parse(localStorage.getItem("currentUser"));
-    let message = "Komm bald wieder "+ user.name+ "!"; 
-    this.notifier.notify( 'success', message );
+    let message = "Komm bald wieder " + user.name + "!";
+    this.notifier.notify('success', message);
     localStorage.clear()
+  }
+
+  isAdmin() {
+    let user: User = JSON.parse(localStorage.getItem("currentUser"));
+    let isAdmin = false;
+    if (user != null) {
+      if (user.email === "lukas.ziefle@web.de") {
+        isAdmin = true;
+      }
+    }
+
+    return isAdmin;
   }
 
 }
